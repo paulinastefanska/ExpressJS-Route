b050b6be07f8ed3e4499dcc5bc6a39902495ff4d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-08-22 13:56:37 +0200

    Add GET endpoint

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var stringifyFile;
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    stringifyFile = data
+	    res.send(data);
+	})
+});
