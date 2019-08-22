0528af2587f99943098c4963adb9189cc56c9d37
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-08-22 14:07:13 +0200

    Add POST endpoint

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ app.get('/getNote', function (req, res) {
 	    res.send(data);
 	})
 });
+
+app.post('/updateNote/:note', function (req, res) {
+	fs.readFile('./test.json', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    stringifyFile = data + req.params.note;
+		fs.writeFile('./test.json', stringifyFile, function(err) {
+		    if (err) throw err;
+		    console.log('file updated');
+		})
+	});
+});
+
+app.listen(3000);
